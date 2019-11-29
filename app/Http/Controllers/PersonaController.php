b3ca7ca0230563d76d2481016caa26e807dbb2da
kettymar <ing.kettyruiz@gmail.com>
2019-11-29 14:11:26 -0500

    primer commit

---
+++ app/Http/Controllers/PersonaController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Persona;
+use DB;
+
+class PersonaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $personas = Persona::all();
+        return $personas->toArray();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+           $puntos = DB::select(" call ConsultarPuntos (".$id.")");
+        //
+           return $puntos;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function consultarNic ($nic){
+
+        $nic = DB::select('select 1 existe from personas where nic = ?', [$nic]);
+
+         if ($nic==null){
+
+            $res = 'NIC no existe';
+
+         }else {
+
+            $res = $nic;
+         }
+
+
+         return $res;
+
+
+
+    }
+}
